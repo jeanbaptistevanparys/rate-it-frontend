198ac3f579fe156464036b979d3a97a1707f7b40
Jean-Baptiste Van Parys <jean-baptiste.van.parys@student.howest.be>
2023-05-11 19:13:00 +0200

    added the correct colors and shadows

--- src/modules/Auth/services/AuthService.js
+++ src/modules/Auth/services/AuthService.js
@@ -1,11 +1,39 @@
 const base_url = 'http://localhost/api/';
 
 export default class AuthService {
-	async login(username, password) {
-		// TODO: implement login
+	async login(email, password) {
+
+		console.log(JSON.stringify({ email, password }));
+
+		let fullUrl = base_url + 'login';
+		const response = await fetch(fullUrl, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ email, password }),
+		});
+		const data = await response.json();
+
+		this.registerToken(data.token);
+
+		return data;
 	}
 
-	async register(username, email, password) {
-		//TODO: implement register
+	async register(name, email, password) {	
+		let fullUrl = base_url + 'register';
+		const response = await fetch(fullUrl, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ name, email, password }),
+		});
+		// TODO: handle errors
+	}
+
+	registerToken(token) {
+		//save token to local storage
+		localStorage.setItem('token', 'Bearer ' + token);
 	}
 }
---
+++ src/modules/Ratables/services/RatableService.js
@@ -0,0 +1,82 @@
+const url = 'http://localhost/api/topic/';
+
+const token = localStorage.getItem('token');
+
+const language = localStorage.getItem('language');
+
+
+// ask casier
+
+export default class TopicService {
+	constructor() {
+		this.page = 1;
+		this.perPage = 10;
+	}
+
+	setPage(page) {
+		this.page = page;
+		return this;
+	}
+
+	async getRatables(topic) {
+
+		let fullUrl = url;
+		fullUrl += topic + '/ratable'  ;
+		// fullUrl += "?perPage=" + this.perPage;
+		// fullUrl += "&page=" + this.page;
+		fullUrl += "?language=" + language;
+
+		const response = await fetch(fullUrl , 
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token,
+                }  
+            });
+		const data = await response.json();
+
+		console.log(data.data);
+
+		return data.data;
+	}
+
+	async rate(topic, ratableId, rating) {
+		// /topic/{topicName}/ratable/{ratableId}/rating
+		// post rating
+		let fullUrl = url;
+		fullUrl += topic + '/ratable/' + ratableId + '/rating';
+
+		const response = await fetch(fullUrl, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: token,
+			},
+			body: JSON.stringify(rating),
+		});
+
+		const data = await response.json();
+
+
+		return data;
+
+		// TODO: handle errors
+	}
+
+	async unrate(topic, ratableId, rating){
+		let fullUrl = url;
+		fullUrl += topic + '/ratable/' + ratableId + '/rating/' + rating.id;
+
+		const response = await fetch(fullUrl, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: token,
+			}
+		});
+
+		const data = await response.json();
+
+		return data;
+	}
+}
