9b51657e2f9c3bd7ebd3fa2480071ef6b7f6d5cc
Jean-Baptiste Van Parys <jean-baptiste.van.parys@student.howest.be>
2023-05-13 18:12:59 +0200

    404 not found

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue')
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/RegisterView.vue')
-    },
-    {
-      path: '/:lang/home',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
-    },
-    {
-      path: '/:lang/topic/:id',
-      name: 'topic',
-      component: () => import('../views/TopicView.vue')
-    },
-  ]
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/login',
+			name: 'login',
+			component: () => import('../views/LoginView.vue'),
+		},
+		{
+			path: '/register',
+			name: 'register',
+			component: () => import('../views/RegisterView.vue'),
+		},
+		{
+			path: '/:lang/home',
+			name: 'home',
+			component: () => import('../views/HomeView.vue'),
+		},
+		{
+			path: '/:lang/topic/:id',
+			name: 'topic',
+			component: () => import('../views/TopicView.vue'),
+		},
+		{ path: '/:pathMatch(.*)*', name: 'notfound', component: () => import('../views/404.vue') },
+	],
+});
 
-export default router
+export default router;
---
+++ src/views/404.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+        <h1>404</h1>
+        <p>Page not found</p>
+    </main>
+</template>
+<script>
+export default {
+    name: 'NotFoundView',
+}
+</script>
+<style>
+main {
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+}
+
+h1 {
+    color: rgba(32, 180, 137, 1);
+    font-size: 10rem;
+    margin: 0;
+}
+
+p {
+    color: rgba(118, 118, 118, 1);
+    font-size: 2rem;
+    margin: 0;
+}
+</style>
