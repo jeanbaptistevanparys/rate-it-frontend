f1ac2ba9c7ec5ee32e6211a4fcf8c4617a7efe8d
jari.valentine <jari.valentine@student.howest.be>
2023-05-21 11:09:18 +0200

    loading hot topics

--- src/modules/Topics/components/HotTopics.vue
+++ src/modules/Topics/components/HotTopics.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="hot-topics">
         <h2>hot topics</h2>
-        <ul>
+        <ul v-if="!loading">
             <li @click="select(topic.id)" v-for="(topic, index) in topics" :key="topic.id">
                 <span>{{ "#" + (index + 1) }}</span>{{ topic.name }}
             </li>
         </ul>
+        <div v-else class="loading"></div>
     </div>
 </template>
 <script>
@@ -18,11 +19,13 @@ export default {
             topics: [],
             "service": new TopicService(),
             route: useRoute(),
-            router: useRouter()
+            router: useRouter(),
+            loading: true
         }
     },
     async mounted() {
         this.topics = await this.service.getHotTopics();
+        this.loading = false;
     },
     methods: {
         select(topicId) {
